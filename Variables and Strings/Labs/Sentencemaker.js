91ce16952b683a934fa2d161bd9411d5a02476a9
Yash Maurya <yashmaurya370@gmail.com>
2026-06-10 16:24:16 +0530

    completed sentence maker lab and organized assets file

---
+++ Variables and Strings/Labs/Sentencemaker.js
@@ -0,0 +1,21 @@
+let adjective = "brave";
+let noun = "dragon";
+let verb = "angry";
+let place = "mountain";
+let adjective2 = "smoky";
+let noun2 = "marshmallows";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "curious";
+noun = "unicorn";
+verb = "excited";
+place = "forest";
+adjective2 = "sparkling";
+noun2 = "cupcakes";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
