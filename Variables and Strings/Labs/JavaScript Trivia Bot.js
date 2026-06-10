2430d1aeed2ccfb994a1b884c2b4df5e0428b270
Yash Maurya <yashmaurya370@gmail.com>
2026-06-10 15:00:23 +0530

    completed a lab and workshop

---
+++ Variables and Strings/Labs/JavaScript Trivia Bot.js
@@ -0,0 +1,26 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "CodeBuddy";
+const botLocation = "the Internet";
+const favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days by Brendan Eich in 1995.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " is one of the core technologies of the web, alongside HTML and CSS.";
+console.log(codingFact);
+
+
+codingFact = favoriteLanguage + " can be used for both front-end and back-end development using environments like Node.js.";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+  botName +
+  " from " +
+  botLocation +
+  "."
+);
